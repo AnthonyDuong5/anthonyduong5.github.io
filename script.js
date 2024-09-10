0c43cae7c5df01634e150c05e33c7cc7503d51f0
AnthonyDuong5 <aduon067@ucr.edu>
2024-09-09 20:41:17 -0700

    Text adjustment and more webpage interactions

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 console.log("Nothing here!");
 let clicks = 0;
-document.getElementById('button').addEventListener('click', function() {
+function buttonCounter() {
     ++clicks;
-    alert('Button click count: ' + clicks);
-});
+    alert("Button click count: " + clicks);
+}
+function noCopy() {
+    document.getElementById("webpage").remove();
+    alert("Are you copying my webpage?");
+}
+function boldDescription() {
+    document.getElementById("description").style.fontWeight = "bold";
+}
+function resetDescription() {
+    document.getElementById("description").style.fontWeight = "normal";
+}
