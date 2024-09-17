6a9965cacabdc9dff3f59066e4109ec9d7bcb523
AnthonyDuong5 <aduon067@ucr.edu>
2024-09-16 19:23:30 -0700

    Animation change, ASCII art, and more

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log("Nothing here!");
 let clicks = 0;
 function buttonCounter() {
-    ++clicks;
-    alert("Button click count: " + clicks);
+    document.getElementById("button").textContent = `Button (Clicks: ${++clicks})`;
 }
 function noCopy() {
     document.getElementById("webpage").remove();
@@ -14,3 +12,32 @@ function boldDescription() {
 function resetDescription() {
     document.getElementById("description").style.fontWeight = "normal";
 }
+const element = document.querySelector(".animate");
+let x = 0;
+let direction = 1;
+function animate() {
+    if (window.innerWidth - 400 > 0) {
+        x += direction * 2;
+        if (x > window.innerWidth - 400 || x < 0) {
+        direction *= -1;
+        }
+        element.style.transform = `translateX(${x}px)`;
+        requestAnimationFrame(animate);
+    }
+}
+function handleResize() {
+    x = Math.min(x, window.innerWidth - 400);
+}
+window.addEventListener("resize", handleResize);
+animate();
+console.log("Secret area found!");
+const smallAlienInvader = `
+\x1b[31m
+  /\\_/\\
+ ( o o )
+  \\_^_/
+  --|--
+   / \\
+\x1b[0m
+`;
+console.log(smallAlienInvader);
